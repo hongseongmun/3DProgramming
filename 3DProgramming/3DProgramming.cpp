e8856f9a13e2e420ab242483331847c34baf025c
BuildTools <unconfigured@null.spigotmc.org>
2022-09-16 11:53:03 +0900

    초기 커밋.

---
+++ 3DProgramming/3DProgramming.cpp
@@ -0,0 +1,100 @@
+﻿#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+//#include <GL/glut.h>
+
+#pragma comment(lib, "OpenGL32")
+
+//void changeSize(int w, int h);
+
+static void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+
+}
+
+
+/* void changeSize(int w, int h)
+{
+    glViewport(0, 0, w, h);
+
+    GLfloat widthFactor = (GLfloat)w / (GLfloat)640;
+    GLfloat heightFactor = (GLfloat)h / (GLfloat)640;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0 * widthFactor, 1.0 * widthFactor, -1.0 * heightFactor, 1.0 * heightFactor, -1.0, 1.0);
+}
+*/
+
+int main(void)
+{
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+    window = glfwCreateWindow(800, 800, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int width, height;
+
+        double radius = 1.0f;
+        double angle = 3.14152 / 180;
+        double angle2 = 3.14152;
+
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float)height;
+        
+        
+        ////90도와 같이 각도로 연산하고 싶으면
+        ////glm의 삼각함수는 radian 형식으로 입력을 받기 때문에 변환해서 넣어주어야한다.
+        //glm::sin(glm::radians(90.0));  
+        
+        glClearColor(0.0f, 0.0f, 0.0f, 0.1f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_LINE_STRIP);
+
+
+       for (int k = 0; k <= 120; k++)
+        {
+            glVertex2f(glm::cos(glm::radians(angle2)*k),glm::sin(glm::radians(angle2)*k));
+       }
+        
+        
+      // for (int i = 0; i <= 360; i++)
+      // {
+      //     glVertex2f(glm::cos(i*angle),glm::sin(i*angle));
+     //  }
+      
+
+        glEnd();
+        //glutReshapeFunc(changeSize);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        
+    }
+    
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    
+    exit(EXIT_SUCCESS);
+}
